049c84d05001d578966c5681e7747a7281b4a970
usiegl00 <50933431+usiegl00@users.noreply.github.com>
2024-10-09 21:17:33 +0900

    Added restart_dynos.

--- src/build-client/api/default_api.cr
+++ src/build-client/api/default_api.cr
@@ -74,61 +74,6 @@ module Build
       return nil, status_code, headers
     end
 
-    # restart dynos
-    # @param id [String] app id or name
-    # @return [nil]
-    def api_v1_apps_id_dynos_delete(id : String)
-      api_v1_apps_id_dynos_delete_with_http_info(id)
-      nil
-    end
-
-    # restart dynos
-    # @param id [String] app id or name
-    # @return [Array<(nil, Integer, Hash)>] nil, response status code and response headers
-    def api_v1_apps_id_dynos_delete_with_http_info(id : String)
-      if @api_client.config.debugging
-        Log.debug {"Calling API: DefaultApi.api_v1_apps_id_dynos_delete ..."}
-      end
-      # verify the required parameter "id" is set
-      if @api_client.config.client_side_validation && id.nil?
-        raise ArgumentError.new("Missing the required parameter 'id' when calling DefaultApi.api_v1_apps_id_dynos_delete")
-      end
-      # resource path
-      local_var_path = "/api/v1/apps/{id}/dynos".sub("{" + "id" + "}", URI.encode_path(id.to_s))
-
-      # query parameters
-      query_params = Hash(String, String).new
-
-      # header parameters
-      header_params = Hash(String, String).new
-
-      # form parameters
-      form_params = Hash(Symbol, (String | ::File)).new
-
-      # http body (model)
-      post_body = nil
-
-      # return_type
-      return_type = nil
-
-      # auth_names
-      auth_names = ["bearer", "oauth2"]
-
-      data, status_code, headers = @api_client.call_api(:DELETE,
-                                                        local_var_path,
-                                                        :"DefaultApi.api_v1_apps_id_dynos_delete",
-                                                        return_type,
-                                                        post_body,
-                                                        auth_names,
-                                                        header_params,
-                                                        query_params,
-                                                        form_params)
-      if @api_client.config.debugging
-        Log.debug {"API called: DefaultApi#api_v1_apps_id_dynos_delete\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"}
-      end
-      return nil, status_code, headers
-    end
-
     # identity
     # @return [ApiV1MeGet200Response]
     def api_v1_me_get()
@@ -622,6 +567,122 @@ module Build
         Log.debug {"API called: DefaultApi#namespaces\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"}
       end
       return Array(Namespace).from_json(data), status_code, headers
+    end
+
+    # restart all dynos
+    # @param id [String] app id or name
+    # @return [nil]
+    def restart_all_dynos(id : String)
+      restart_all_dynos_with_http_info(id)
+      nil
+    end
+
+    # restart all dynos
+    # @param id [String] app id or name
+    # @return [Array<(nil, Integer, Hash)>] nil, response status code and response headers
+    def restart_all_dynos_with_http_info(id : String)
+      if @api_client.config.debugging
+        Log.debug {"Calling API: DefaultApi.restart_all_dynos ..."}
+      end
+      # verify the required parameter "id" is set
+      if @api_client.config.client_side_validation && id.nil?
+        raise ArgumentError.new("Missing the required parameter 'id' when calling DefaultApi.restart_all_dynos")
+      end
+      # resource path
+      local_var_path = "/api/v1/apps/{id}/dynos".sub("{" + "id" + "}", URI.encode_path(id.to_s))
+
+      # query parameters
+      query_params = Hash(String, String).new
+
+      # header parameters
+      header_params = Hash(String, String).new
+
+      # form parameters
+      form_params = Hash(Symbol, (String | ::File)).new
+
+      # http body (model)
+      post_body = nil
+
+      # return_type
+      return_type = nil
+
+      # auth_names
+      auth_names = ["bearer", "oauth2"]
+
+      data, status_code, headers = @api_client.call_api(:DELETE,
+                                                        local_var_path,
+                                                        :"DefaultApi.restart_all_dynos",
+                                                        return_type,
+                                                        post_body,
+                                                        auth_names,
+                                                        header_params,
+                                                        query_params,
+                                                        form_params)
+      if @api_client.config.debugging
+        Log.debug {"API called: DefaultApi#restart_all_dynos\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"}
+      end
+      return nil, status_code, headers
+    end
+
+    # restart specific dynos
+    # @param id [String] app id or name
+    # @param dyno [String] dyno name
+    # @return [nil]
+    def restart_dynos(id : String, dyno : String)
+      restart_dynos_with_http_info(id, dyno)
+      nil
+    end
+
+    # restart specific dynos
+    # @param id [String] app id or name
+    # @param dyno [String] dyno name
+    # @return [Array<(nil, Integer, Hash)>] nil, response status code and response headers
+    def restart_dynos_with_http_info(id : String, dyno : String)
+      if @api_client.config.debugging
+        Log.debug {"Calling API: DefaultApi.restart_dynos ..."}
+      end
+      # verify the required parameter "id" is set
+      if @api_client.config.client_side_validation && id.nil?
+        raise ArgumentError.new("Missing the required parameter 'id' when calling DefaultApi.restart_dynos")
+      end
+      # verify the required parameter "dyno" is set
+      if @api_client.config.client_side_validation && dyno.nil?
+        raise ArgumentError.new("Missing the required parameter 'dyno' when calling DefaultApi.restart_dynos")
+      end
+      # resource path
+      local_var_path = "/api/v1/apps/{id}/dynos/{dyno}".sub("{" + "id" + "}", URI.encode_path(id.to_s)).sub("{" + "dyno" + "}", URI.encode_path(dyno.to_s))
+
+      # query parameters
+      query_params = Hash(String, String).new
+
+      # header parameters
+      header_params = Hash(String, String).new
+
+      # form parameters
+      form_params = Hash(Symbol, (String | ::File)).new
+
+      # http body (model)
+      post_body = nil
+
+      # return_type
+      return_type = nil
+
+      # auth_names
+      auth_names = ["bearer", "oauth2"]
+
+      data, status_code, headers = @api_client.call_api(:DELETE,
+                                                        local_var_path,
+                                                        :"DefaultApi.restart_dynos",
+                                                        return_type,
+                                                        post_body,
+                                                        auth_names,
+                                                        header_params,
+                                                        query_params,
+                                                        form_params)
+      if @api_client.config.debugging
+        Log.debug {"API called: DefaultApi#restart_dynos\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"}
+      end
+      return nil, status_code, headers
     end
 
     # set or update config-vars
